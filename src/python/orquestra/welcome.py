3b87ca0a69a69eed8b36cc7a68cc0c75931e48ff
mlxd <loriordan@gmail.com>
2020-04-29 15:14:10 +0100

    Added a new method

--- src/python/orquestra/welcome.py
+++ src/python/orquestra/welcome.py
@@ -3,13 +3,19 @@ This module saves a welcome message.
 """
 
 import json
+import numpy as np
 
 def welcome():
+    a = np.array(range(1000))
+    b = np.array(range(1000))
     message = "Welcome to Orquestra!"
 
     message_dict = {}
-    message_dict["message"] = message
+    message_dict["message"] = calc_vals(a,b)
     message_dict["schema"] = "message"
 
     with open("welcome.json",'w') as f:
         f.write(json.dumps(message_dict, indent=2)) # Write message to file as this will serve as output artifact
+
+def calc_vals(a, b):
+    return np.sin(a)*np.cos(b)
